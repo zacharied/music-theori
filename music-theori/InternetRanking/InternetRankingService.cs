7a5f9f8159156e7b1a0a75d0732c1d7ce66bbafc
Zacharie Day <zachdayz@gmail.com>
2020-09-17 16:40:00 -0400

    Give scripting access to Axis enum

---
+++ music-theori/InternetRanking/InternetRankingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using theori.Database;
+using theori.Scoring;
+
+namespace theori.InternetRanking
+{
+    public abstract class InternetRankingService
+    {
+        private HttpClient m_client;
+
+        public InternetRankingService(HttpClient client)
+        {
+            m_client = client;
+        }
+
+        public void SubmitScore(string token, Dictionary<string, object> values)
+        {
+        }
+    }
+}
